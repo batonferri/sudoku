a42d38981fd34875039a957b8c388bad7aa85329
Baton Ferri <ferribaton@gmail.com>
2022-12-02 18:30:57 +0100

    added vercel config file

--- sudokuAPI/index.js
+++ sudokuAPI/index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use("/api/solve", solveRoutes);
 
-app.listen(5000, () => {
-  console.log("http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running in port: ${PORT}`);
 });
